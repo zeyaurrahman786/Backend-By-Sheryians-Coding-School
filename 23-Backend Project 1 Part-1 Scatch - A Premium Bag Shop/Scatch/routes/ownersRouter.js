f91fd31b3ffe4b117392d594e24d860c3d850d90
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-05-23 09:07:25 +0530

    Backend Project

--- 23-Backend Project 1 Part-1 Scatch - A Premium Bag Shop/Scatch/routes/ownersRouter.js
+++ 23-Backend Project 1 Part-1 Scatch - A Premium Bag Shop/Scatch/routes/ownersRouter.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require("../models/owner-model")
+
+
+if(process.env.NODE_ENV === "development"){
+router.post('/create', async (req, res) => {
+    let owners = await ownerModel.find();
+    if(owners.length > 0){
+        return res.status(500).send("You don't have permission to create a new owner")
+    }
+
+    let {fullname, email, password} = req.body;
+    let createdOwner = await ownerModel.create({
+        fullname,
+        email,
+        password,
+    });
+
+    res.status(201).send(createdOwner);
+});
+}
 
 router.get('/', (req, res) => {
     res.send("Hey it's working Owners Testing");
